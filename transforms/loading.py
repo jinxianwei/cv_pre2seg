ba73d5b4d7507a92db39dc02c428684c1fc257ac
Bennie.jin <Bennie.jin@wellruntech.com>
2023-07-06 09:44:20 +0800

    get file size

--- transforms/loading.py
+++ transforms/loading.py
@@ -1,7 +1,20 @@
 import numpy as np
 import cv2
-from typing import Optional
+from PIL import Image
+from typing import Optional, Dict
 import io
+
+MODEL_PALETTE = {
+    '1': 1,
+    'L': 8,
+    'P': 8,
+    'RGB': 24,
+    'RGBA': 32,
+    'CMYK': 32, 
+    'YCbCr': 24,
+    'I': 32,
+    'F': 32
+}
 
 class LoadImageFromFile():
     """Load an image from file.
@@ -38,16 +51,21 @@ class LoadImageFromFile():
 
         try:
             img_path = self.img_path
-            # TODO the png of channel is 4, , cv2.IMREAD_UNCHANGED分情况是否有必要, tiff的数值范围会有影响
-            # img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+
             
-            with open(img_path, 'rb') as f:
-                img_buff = f.read()
-            img_b = io.BytesIO(img_buff).read()
-            bit_size = len(img_b)
+
+            img_pil = Image.open(img_path)
+            temp = img_pil.getbands()
+            model = ''
+            for i in range(len(temp)):
+                model += temp[i]
+            # TODO 异常
+            bitdepth = MODEL_PALETTE[model]
+            results['BitDepth'] = bitdepth
 
 
-            img = cv2.imread(img_path)
+
+            img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
             
             # cv2.imshow('img', img)
             # cv2.waitKey(0)
@@ -72,15 +90,25 @@ class LoadImageFromFile():
         results['dtype'] = img.dtype
         results['ndim'] = img.ndim
         results['nbytes'] = img.nbytes
-        results['BitDepth'] = str(results['dtype'])[-1]
 
         results['element_size'] = img.size
         results['itemsize'] = img.itemsize
 
+        
+
+        return results
+
+
+    def get_filesize(self, 
+                     img_path: str,
+                     results: Dict)-> Optional[dict]:
+        """
+        get img file size
+        """
+        with open(img_path, 'rb') as f:
+            img_buff = f.read()
+        img_b = io.BytesIO(img_buff).read()
+        bit_size = len(img_b)
         results['FileSize'] = bit_size
-
-
-
-
 
         return results
